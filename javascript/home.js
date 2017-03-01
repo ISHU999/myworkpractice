3156a68b9d5e3317df71d9216082bddc6570f6c6
unknown <agolan@BLRVALA17.sapient.com>
2017-03-01 09:10:57 +0530

    Updating with changes to home.html

--- javascript/home.js
+++ javascript/home.js
@@ -1,6 +1,6 @@
-(function(){
-	var delete_element=document.getElementById('delete_button');
-	var delete_modal=document.getElementById('container_modal');
+(function (){
+	var delete_element = document.getElementById('delete_button');
+	var delete_modal = document.getElementById('container_modal');
 	delete_element.addEventListener('click',function(){
 		delete_modal.setAttribute('style','display:flex');
 	})
--- javascript/modal.js
+++ javascript/modal.js
@@ -1,7 +1,7 @@
 (function(){
-	var modal_view=document.getElementById('container_modal');
-	var delete_yes=document.getElementById('modal_delete_btn');
-	var delete_no=document.getElementById('modal_delete_btn_deny');
+	var modal_view= document.getElementById('container_modal');
+	var delete_yes= document.getElementById('modal_delete_btn');
+	var delete_no= document.getElementById('modal_delete_btn_deny');
 
 	delete_yes.addEventListener('click',function(){
 		console.log('yes is working');

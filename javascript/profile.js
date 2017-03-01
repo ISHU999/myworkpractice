bb2381f5e9934e333823a81b20b2f08ada6f00a3
Amartya Sarkar <asark5@sapient.com>
2017-03-01 17:13:33 +0530

    updated profile'

--- javascript/profile.js
+++ javascript/profile.js
@@ -1,16 +1,14 @@
 
-var show =	document.getElementsByClassName('down-array')[0];
-console.log(show)
-for(var i=0 ; i<show.size ;i++)
-	show[i].addEventListener('click', show_sublist);
+var show =	document.getElementById('show');
+show.addEventListener('click', show_sublist);
 
 function show_sublist(){
 	console.log('called')
 	var sublist = document.getElementsByClassName('sub-list-group');
-	console.log(sublist);
+	console.log(sublist[0]);
 
-	sublist[1].setAttribute('style', 'display:block');
-	
+	//sublist[1].setAttribute('style', 'display:block');
+	sublist[0].className+=('show');	
 }
 
 
